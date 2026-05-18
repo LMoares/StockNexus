8e0fae2c8f4057fcebcf8da3e4fb061bb5755a62
Lorenzo <moares.lorenzo@gmail.com>
2026-05-18 12:17:23 +0100

    deleted redundant registeruserdto - replaced by registration handler, added products - command, handler, and validation, created stock file to store products, migrated and updated database

--- StockNexusAPI/Application/DTOs/Users/UserDto.cs
+++ StockNexusAPI/Application/DTOs/Users/UserDto.cs
@@ -1,5 +1,7 @@
 ﻿using StockNexusAPI.Domain.Enums;
-
+// This DTO is used to transfer user data between the application layers and generation of JWT tokens.
+// The UserRole enumeration defines user roles such as: Admin, Manager, and Employee
+// Roles are used to control access to certain features and endpoints in the application
 namespace StockNexusAPI.Application.DTOs.Users
 {
     public class UserDto
--- StockNexusAPI/Infrastructure/Persistence/ApplicationDbContext.cs
+++ StockNexusAPI/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -10,6 +10,10 @@ namespace StockNexusAPI.Infrastructure.Persistence
 
         public DbSet<User> Users { get; set; }
 
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Stock> Stock { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
